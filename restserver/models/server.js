80d1017490c7367a3cc1da51661aa89a9b5e4677
codingjhames <jhames.mejia@unipamplona.edu.co>
2024-08-06 14:15:22 -0500

    restserver-DBconnection

--- restserver/models/server.js
+++ restserver/models/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { dbConnection } = require("../database/config");
 
 class Server {
   constructor() {
@@ -7,10 +8,17 @@ class Server {
     this.port = process.env.PORT;
     this.usuariosPath = "/api/usuarios";
 
+    //Conectar a base de datos
+    this.conectarDB();
+
     // Middlewares -> funciones que se ejecutan al iniciar el server
     this.middlewares();
     // Rutas de la app
     this.routes();
+  }
+
+  async conectarDB() {
+    dbConnection();
   }
 
   middlewares() {
